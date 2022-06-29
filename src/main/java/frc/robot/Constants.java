2a3788e1b575f219d6a22a0dd2cd97cc7d15ee9b
Jason Weick II <98281586+BatmanBetta@users.noreply.github.com>
2022-06-29 14:38:50 -0500

    Ignore this
    I SAID TO IGNORE THIS

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -50,11 +50,11 @@ public static final int LED_PORT_B = 1;
 public static final int TURRET_MOTOR_ID = 0;
 
 // Drive
- public static final double DRIVE_Y = 1;
- public static final double DRIVE_ROT = 1;
- public static final int LEFT_FRONT_MOTOR = 0;
- public static final int LEFT_REAR_MOTOR = 1;  
- public static final int RIGHT_FRONT_MOTOR = 2; 
- public static final int RIGHT_REAR_MOTOR = 3; 
+public static final double DRIVE_Y = .9;
+public static final double DRIVE_ROT = .9;
+public static final int LEFT_FRONT_MOTOR = 0;
+public static final int LEFT_REAR_MOTOR = 1;  
+public static final int RIGHT_FRONT_MOTOR = 2; 
+public static final int RIGHT_REAR_MOTOR = 3; 
 
 }
